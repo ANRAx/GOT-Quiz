f6399ee5ec4d9f997f54e8622ac8c29a0d6e3261
ANRAx <ramdassaj@gmail.com>
2019-11-25 12:51:33 -0500

    create card question var, countStart var, and questions array

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,65 @@
+var card = $("#quiz-area");
+var countStartNumber = 30;
+
+// Question Set 
+
+var questions = [{
+
+    questions: "",
+    answers: [""],
+    correctAnswer: "",
+    image: ""
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}, {
+    questions = "",
+    answers: [""],
+    correctAnswer: "",
+    image: "",
+}];
+
+var timer;
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    
+}
